524227d35a59e77e49b62c5ef7ef9add35239d92
Joshua Ganter <mail@joshuaganter.de>
2025-04-13 21:36:25 +0200

    feat: Add first implementation of Filter()

--- functional/filter.go
+++ functional/filter.go
@@ -1,1 +1,5 @@
 package functional
+
+func Filter(elements []int, filterFn func(int) bool) []int {
+	return []int{1, 2, 3}
+}
--- functional/filter_test.go
+++ functional/filter_test.go
@@ -1,1 +1,28 @@
 package functional_test
+
+import (
+	"testing"
+
+	"github.com/JoshuaGanter/go-concurrency/functional"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFilter(t *testing.T) {
+	tests := []struct {
+		input    []int
+		filterFn func(int) bool
+		expected []int
+	}{
+		{
+			input:    []int{1, 2, 3, 4, 5},
+			filterFn: func(elem int) bool { return elem < 4 },
+			expected: []int{1, 2, 3},
+		},
+	}
+
+	for _, test := range tests {
+		var actual []int = functional.Filter(test.input, test.filterFn)
+
+		assert.ElementsMatch(t, test.expected, actual)
+	}
+}
